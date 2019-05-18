8cc91a21dc3f62e5e28b53e09cb51fb842f099c2
wesselrijk <wesselrijk@gmail.com>
2019-05-18 21:10:07 +0200

    Done up till 2. Showing the menu, due to unexpected response code 404 for https://resto.mprog.nl/ and com.android.volley.ClientError

--- app/src/main/java/com/example/restaurant/CategoriesRequest.java
+++ app/src/main/java/com/example/restaurant/CategoriesRequest.java
@@ -1,11 +1,25 @@
 package com.example.restaurant;
 
 import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
-public class CategoriesRequest  {
+public class CategoriesRequest implements Response.Listener<JSONObject>, Response.ErrorListener  {
 
     private Context context;
+    private Callback activity;
 
     public CategoriesRequest(Context context) {
         this.context = context;
@@ -17,6 +31,41 @@ public class CategoriesRequest  {
     }
 
     public void getCategories(Callback activity) {
+        this.activity = activity;
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://resto.mprog.nl/", null, this, this);
+        queue.add(jsonObjectRequest);
+    }
 
+    @Override
+    public void onResponse(JSONObject response) {
+
+        JSONArray categories = null;
+        try {
+            categories = response.getJSONArray("categories");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d("onResponse error message.",e.getMessage());
+            activity.gotCategoriesError(e.getMessage());
+        }
+
+        ArrayList<String> categoriesList = new ArrayList<>();
+        for (int i = 0; i < categories.length(); i++) {
+            try {
+                categoriesList.add(categories.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.d("onResponse error message.",e.getMessage());
+                activity.gotCategoriesError(e.getMessage());
+            }
+        }
+        activity.gotCategories(categoriesList);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        activity.gotCategoriesError(error.toString());
+        Log.d("Volley error message.",error.toString());
     }
 }
--- app/src/main/java/com/example/restaurant/MainActivity.java
+++ app/src/main/java/com/example/restaurant/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.restaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CategoriesRequest.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CategoriesRequest requestCategories = new CategoriesRequest(this);
+        requestCategories.getCategories(this);
+        Toast.makeText(this,"Started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void gotCategories(ArrayList<String> categories) {
+        Log.d("got category", categories.get(0));
+        Toast.makeText(this,categories.get(0), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void gotCategoriesError(String message) {
+        Log.d("got error", message);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
